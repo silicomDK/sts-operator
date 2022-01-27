a3df68af9e55ce725aca109b5c15d956339c13b3
rmr <rmr@silicom.dk>
2022-01-27 11:37:49 +0100

    Do not create default operator CR config.

--- main.go
+++ main.go
@@ -17,20 +17,14 @@ limitations under the License.
 package main
 
 import (
-	"context"
 	"flag"
-	"fmt"
 	"os"
 
 	nfdv1 "github.com/openshift/cluster-nfd-operator/api/v1"
-	"k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/apimachinery/pkg/types"
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
-	"k8s.io/client-go/rest"
 	ctrl "sigs.k8s.io/controller-runtime"
-	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/healthz"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 
@@ -105,8 +99,6 @@ func main() {
 	}
 	//+kubebuilder:scaffold:builder
 
-	createDefaultOperatorConfig(ctrl.GetConfigOrDie())
-
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
 		setupLog.Error(err, "unable to set up health check")
 		os.Exit(1)
@@ -121,34 +113,4 @@ func main() {
 		setupLog.Error(err, "problem running manager")
 		os.Exit(1)
 	}
-}
-
-func createDefaultOperatorConfig(cfg *rest.Config) error {
-
-	logger := setupLog.WithName("createDefaultOperatorConfig")
-	c, err := client.New(cfg, client.Options{Scheme: scheme})
-	if err != nil {
-		return err
-	}
-
-	config := &stsv1alpha1.StsOperatorConfig{}
-	config.Namespace = defaultNamespace
-	config.Name = defaultName
-
-	err = c.Get(context.TODO(), types.NamespacedName{
-		Name: defaultName, Namespace: defaultNamespace}, config)
-
-	if err != nil {
-		if errors.IsNotFound(err) {
-			logger.Info("Create default OperatorConfig")
-			err = c.Create(context.TODO(), config)
-			if err != nil {
-				fmt.Println(fmt.Printf("Couldn't create OperatorConfig: %v", err))
-				return err
-			}
-		}
-
-		return err
-	}
-	return nil
 }
